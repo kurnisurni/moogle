f130ebefd2b276d611e71bdaafeb53275a58dc4b
Kurnia <kurniasuryani@gmail.com>
2021-05-15 19:48:40 +0200

    Trending page & movie card

--- src/Pages/Trending/Trending.jsx
+++ src/Pages/Trending/Trending.jsx
@@ -1,7 +1,38 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Card from '../../components/Card/Card';
+import './Trending.css';
+
 const Trending = () => {
+    const [content, setContent] = useState([]);
+
+    const fetchTrending = async () => {
+        const { data } = await axios.get(
+            `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}`
+        );
+        // console.log(data.results);
+        setContent(data.results);
+    };
+
+    useEffect(() => {
+        fetchTrending();
+        // eslint-disable-next-line
+    });
+     
     return (
         <div>
-            This is Trending page
+            <span className="pageTitle">Trending</span>
+            <div className="trending">
+                {content && content.map((c) => <Card 
+                key={c.id} 
+                id={c.id} 
+                poster={c.poster_path}
+                title={c.title || c.name}
+                date={c.first_air_date || c.release_date}
+                media_type={c.media_type}
+                vote_average={c.vote_average}
+                />)}
+            </div>
         </div>
     );
 };
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,30 @@
+import { Badge } from "@material-ui/core";
+import { img_300 } from "../../config/config";
+import "./Card.css";
+
+const Card = ({ 
+    poster, 
+    title, 
+    date, 
+    media_type, 
+    vote_average 
+}) => {
+
+  return (
+    <div className="media">
+      <Badge
+        badgeContent={vote_average}
+        color={vote_average > 6 ? "primary" : "secondary"}
+      />
+      <img className="poster" src={`${img_300}${poster}`} alt={title} />
+
+      <b className="title">{title}</b>
+      <span className="subTitle">
+        {media_type === "tv" ? "TV Series" : "Movie"}
+        <span className="subTitle">{date}</span>
+      </span>
+    </div>
+  );
+};
+
+export default Card;
