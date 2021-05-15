2fa18a9131ff39492c765a6565178136e96c7f68
Kurnia <kurniasuryani@gmail.com>
2021-05-15 20:25:20 +0200

    Genres component

--- src/Pages/Movies/Movies.jsx
+++ src/Pages/Movies/Movies.jsx
@@ -2,11 +2,15 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Card from '../../components/Card/Card';
 import CustomPagination from '../../components/Pagination/CustomPagination';
+import Genres from '../../components/Genres/Genres';
 
 const Movies = () => {
+
     const [page, setPage] = useState(1);
     const [content, setContent]= useState([]);
     const [numOfPages, setNumOfPages]= useState();
+    const [selectedGenres, setSelectedGenres] = useState([]);
+    const [genres, setGenres] = useState([]);
 
     const fetchMovies = async () => {
         const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`);
@@ -24,6 +28,14 @@ const Movies = () => {
     return (
         <div>
             <span className="pageTitle">Movies</span>
+            <Genres 
+            type="movie" 
+            selectedGenres={selectedGenres} 
+            setSelectedGenres={setSelectedGenres} 
+            genres={genres} 
+            setGenres={setGenres} 
+            setPage={setPage}
+            />
             <div className="trending">
                 {content && content.map((c) => <Card 
                 key={c.id} 
@@ -33,8 +45,7 @@ const Movies = () => {
                 date={c.first_air_date || c.release_date}
                 media_type="movie"
                 vote_average={c.vote_average}
-                />)}
-                
+                />)}                
             </div>
             {numOfPages > 1 && (
                 <CustomPagination setPage={setPage} numOfPages={numOfPages} />
